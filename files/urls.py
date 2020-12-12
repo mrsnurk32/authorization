78f25e972aa772d29199a78bae95dd8f0d2b16e7
mrsnurk32 <nickbelik@mail.ru>
2020-12-12 19:27:20 +0300

    file viewer path added

--- files/urls.py
+++ files/urls.py
@@ -5,8 +5,11 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
+
     path('' , views.home, name = 'main'),
     path('accounts/login/', views.login_view, name = 'login_form'),
     path('register/', views.register, name = 'register'),
-    path('logout',views.logout_view, name = 'logout')
+    path('logout',views.logout_view, name = 'logout'),
+    path('<file_id>/view', views.view_file, name = 'view_file')
+
 ]
