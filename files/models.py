229ff5472803baf85177def42fc8a5d6ee105d94
mrsnurk32 <nickbelik@mail.ru>
2020-12-12 18:04:52 +0300

    datefield - added id field - added

--- files/models.py
+++ files/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from datetime import datetime as dt
 # Create your models here.
 class FileStorage(models.Model):
 
+
+    file_id = models.AutoField(primary_key=True)
     file_name = models.CharField(max_length=120)
+    created_date = models.DateTimeField(auto_now_add=True, blank = True)
     upload = models.FileField(upload_to = 'uploads/')
