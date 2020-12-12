4fd730f0868c82c339e75eba5197735bb001f788
mrsnurk32 <nickbelik@mail.ru>
2020-12-12 16:21:39 +0300

    File model registred

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import FileStorage
 # Register your models here.
+
+@admin.register(FileStorage)
+class AdminFiles(admin.ModelAdmin):
+    model = FileStorage
+    list_display = ('file_name','upload')
+
